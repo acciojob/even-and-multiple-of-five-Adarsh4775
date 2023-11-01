90322b73a5e31bf5381a607e8d408c37fcc4b22c
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-01 20:13:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function checkNumber(num) {
   //your code here
+	if( num > 0 || num < 0 || num === 0)
+	{
+		if(num % 2 === 0 && num % 5 === 0)
+		{
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	if(isNaN(num))
+	{
+		return false;
+	}
+	else{
+		return true;
+	}
+	
 }
-
+ 
 const num = parseInt(prompt("Enter a number."));
-alert(checkNumber(num));
+alert(checkNumber(num)); 
+
+              
